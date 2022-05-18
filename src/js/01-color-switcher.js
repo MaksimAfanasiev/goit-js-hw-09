18ff843da7056cf74d67e68b93fc69b739591f65
MaksimAfanasiev <m_afanasyev@ukr.net>
2022-05-18 18:06:01 +0300

    task1 start

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  bodyEl: document.querySelector('body'),
+};
 
+refs.startBtn.addEventListener('click', onStartClick);
+refs.stopBtn.addEventListener('click', onStopClick);
+
+let intervalId = null;
+
+function onStartClick() {
+  cangeBackgroundColor();
+  intervalId = setInterval(() => cangeBackgroundColor(), 1000);
+}
+
+function onStopClick() {
+  clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function cangeBackgroundColor() {
+  refs.bodyEl.style.backgroundColor = getRandomHexColor();
+}
