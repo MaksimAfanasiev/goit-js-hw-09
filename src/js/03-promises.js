817ae77ba9e11d1d59bf8e3785fa2b19b4f7564a
MaksimAfanasiev <m_afanasyev@ukr.net>
2022-05-21 00:04:12 +0300

    Task3
    Add Task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,48 @@
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('.form');
+const delayInpEl = document.querySelector('input[name="delay"]');
+const stepInpEl = document.querySelector('input[name="step"]');
+const amountInpEl = document.querySelector('input[name="amount"]');
+
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  let currentDelay = Number(delayInpEl.value);
+
+  for (let i = 1; i <= Number(amountInpEl.value); i += 1) {
+    if (i !== 1) {
+      currentDelay += Number(stepInpEl.value);
+    }
+
+    createPromise(i, currentDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({
+          position,
+          delay,
+        });
+      } else {
+        reject({
+          position,
+          delay,
+        });
+      }
+    }, delay);
+  });
+
+  return promise;
 }
