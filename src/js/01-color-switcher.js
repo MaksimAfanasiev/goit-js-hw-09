fb87a559dfd5bef9debd9994096ac4f915dc3b97
MaksimAfanasiev <m_afanasyev@ukr.net>
2022-05-21 03:00:47 +0300

    Styles
    Add css styles

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -4,12 +4,15 @@ const refs = {
   bodyEl: document.querySelector('body'),
 };
 
+refs.stopBtn.setAttribute('disabled', true);
+
 refs.startBtn.addEventListener('click', onStartClick);
 refs.stopBtn.addEventListener('click', onStopClick);
 
 let intervalId = null;
 
 function onStartClick(e) {
+  refs.stopBtn.removeAttribute('disabled');
   refs.startBtn.setAttribute('disabled', true);
   cangeBackgroundColor();
   intervalId = setInterval(() => cangeBackgroundColor(), 1000);
@@ -17,6 +20,7 @@ function onStartClick(e) {
 
 function onStopClick() {
   refs.startBtn.removeAttribute('disabled');
+  refs.stopBtn.setAttribute('disabled', true);
   clearInterval(intervalId);
 }
 
